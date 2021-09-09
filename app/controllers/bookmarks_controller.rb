00c883f89a817fc80d94e4eb3b41dbf8b481d8f3
Efe Arslan <effecarslan@gmail.com>
2021-09-09 16:57:01 +0300

    built the bookmark classes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,30 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show'
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
 end
